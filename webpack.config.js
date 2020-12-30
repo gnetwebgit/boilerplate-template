aca4f7ca0b1811ccb48d6537883e8de914c0c176
Gurpal <gurpal.gwebs@gmail.com>
2020-12-30 14:51:49 +0530

    Add jquery

--- webpack.config.js
+++ webpack.config.js
@@ -7,112 +7,113 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // Here you write different options and tell Webpack what to do
 module.exports = {
 
-  // Path to your entry point. From this file Webpack will begin his work
-  entry: './src/javascript/index.js',
+    // Path to your entry point. From this file Webpack will begin his work
 
-  // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
-  output: {
-    publicPath: '',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js',
-    sourceMapFilename: '[file].map'
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        // Apply rule for .sass, .scss or .css files
-        test: /\.(sa|sc|c)ss$/,
-  
-        // Set loaders to transform files.
-        // Loaders are applying from right to left(!)
-        // The first loader will be applied after others
-        use: [
-            MiniCssExtractPlugin.loader,
+    entry: {
+        app: './src/js/index.js',
+    },
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+        publicPath: '',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
             {
-                // This loader resolves url() and @imports inside CSS
-                loader: "css-loader",
-                options: { sourceMap: true },
-            },
-            {
-                // Then we apply postCSS fixes like autoprefixer and minifying
-                loader: "postcss-loader",
-                options: { sourceMap: true },
-            },
-            {
-                // First we transform SASS to standard CSS
-                loader: "sass-loader",
-                options: {
-                    sourceMap: true,
-                    implementation: require("sass"),
-                    // sassOptions: {
-                    //     outputStyle: "compressed",
-                    // },
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                // Apply rule for .sass, .scss or .css files
+                test: /\.(sa|sc|c)ss$/,
+
+                // Set loaders to transform files.
+                // Loaders are applying from right to left(!)
+                // The first loader will be applied after others
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        // This loader resolves url() and @imports inside CSS
+                        loader: "css-loader",
+                        options: { sourceMap: true },
+                    },
+                    {
+                        // Then we apply postCSS fixes like autoprefixer and minifying
+                        loader: "postcss-loader",
+                        options: { sourceMap: true },
+                    },
+                    {
+                        // First we transform SASS to standard CSS
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                            implementation: require("sass"),
+                            // sassOptions: {
+                            //     outputStyle: "compressed",
+                            // },
+                        }
+                    }
+                ]
+            },
+            {
+                // Now we apply rule for images
+                test: /\.(png|jpe?g|gif|webp|svg)$/i,
+                exclude: /(fonts)/,
+                use: [
+                    {
+                        // Using file-loader for these files
+                        loader: "file-loader",
+                        // In options we can set different things like format
+                        // and directory to save
+                        options: {
+                            outputPath: 'images',
+                            name: '[name].[ext]',
+                        }
+                    }
+                ]
+            },
+            {
+                // Apply rule for fonts files
+                test: /\.(woff|woff2|ttf|otf|eot|svg)$/i,
+                exclude: /(images)/,
+                use: [
+                    {
+                        // Using file-loader too
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'fonts',
+                            name: '[name].[ext]',
+                        }
+                    }
+                ]
             }
         ]
-      },
-      {
-        // Now we apply rule for images
-        test: /\.(png|jpe?g|gif|svg|WebP)$/,
-        use: [
-               {
-                 // Using file-loader for these files
-                 loader: "file-loader",
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+            chunkFilename: '[id].css'
+        })
+    ],
+    devServer: {  // configuration for webpack-dev-server
+        contentBase: './dist',  //source of static assets
+        port: 7700, // port to run dev-server
+        hot: false,
+        inline: false,
+    },
 
-
-                 // In options we can set different things like format
-                 // and directory to save
-                 options: {
-                   outputPath: 'images',
-                   name: '[name].[ext]',
-                 }
-               }
-             ]
-      },
-      {
-        // Apply rule for fonts files
-        test: /\.(woff|woff2|ttf|otf|eot|svg)$/,
-        use: [
-               {
-                 // Using file-loader too
-                 loader: "file-loader",
-                 options: {
-                   outputPath: 'fonts',
-                   name: '[name].[ext]',
-                 }
-               }
-             ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-      chunkFilename: '[id].css'
-    })
-  ],
-  devServer: {  // configuration for webpack-dev-server
-    contentBase: './dist',  //source of static assets
-    port: 7700, // port to run dev-server
-    hot: false,
-    inline: false,
-  },
-
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
-  // minifying and other thing so let's set mode to development
-  mode: 'development'
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on final bundle. For now we don't need production's JavaScript
+    // minifying and other thing so let's set mode to development
+    mode: 'development'
 };
